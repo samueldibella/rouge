c594f235651c25cb5d3fd04dc58c87160b9be7d1
Samuel DiBella <samuel.dibella@gmail.com>
2015-09-19 16:20:11 -0400

    tile texture

--- Assets/Scripts/Scripts/MainCamera.cs
+++ Assets/Scripts/Scripts/MainCamera.cs
@@ -7,13 +7,14 @@ public class MainCamera : MonoBehaviour {
 	GameObject p2;
 	Vector2 lerp;
 	Vector3 destination;
+	public float cameraSpeed;
 
 	// Use this for initialization
 	void Start () {
 		p1 = transform.parent.gameObject.GetComponent<GameState>().playerOne;
 		p2 = transform.parent.gameObject.GetComponent<GameState>().playerTwo;
 		lerp = Vector2.Lerp(p1.transform.position, p2.transform.position, .5f);
-
+		cameraSpeed = .3f;
 		transform.position = new Vector3(lerp.x, lerp.y, transform.position.z);
 	}
 
@@ -32,6 +33,6 @@ public class MainCamera : MonoBehaviour {
 		}
 
 		destination = new Vector3(lerp.x, lerp.y, transform.position.z);
-		transform.position = Vector3.Lerp(transform.position, destination, .2f * Time.deltaTime);
+		transform.position = Vector3.Lerp(transform.position, destination, cameraSpeed * Time.deltaTime);
 	}
 }
